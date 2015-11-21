5c39303fb620a7708334267727bd8d6d996a4d6a
Bharad Raghavan <bharadr@napoli110.Stanford.EDU>
2015-11-20 20:03:52 -0800

    Removed torch from being tracked, deleted your torch.ORIG, and modified gitignore

--- torch/share/lua/5.1/cunn/init.lua
+++
@@ -1,9 +0,0 @@
-require "cutorch"
-require "nn"
-require "libcunn"
-
-include('test.lua')
-
-include('DataParallelTable.lua')
-
-nn.Module._flattenTensorBuffer['torch.CudaTensor'] = torch.FloatTensor.new
