167c08737250de0b16bc667338a4f104d601cc37
Bryan Anenberg <anenberg@napoli110.Stanford.EDU>
2015-11-20 20:06:05 -0800

    not tracking torch

--- torch/share/cmake/torch/THConfig.cmake
+++
@@ -1,9 +0,0 @@
-# Find the TH includes and library
-#
-# TH_INCLUDE_DIR -- where to find the includes
-# TH_LIBRARIES -- list of libraries to link against
-# TH_FOUND -- set to 1 if found
-
-SET(TH_FOUND 1)
-SET(TH_INCLUDE_DIR "/afs/cs.stanford.edu/u/anenberg/scr/torch/DeepMind-Atari-Deep-Q-Learner/torch/include/TH")
-SET(TH_LIBRARIES "/afs/cs.stanford.edu/u/anenberg/scr/torch/DeepMind-Atari-Deep-Q-Learner/torch/lib/libTH.so")
